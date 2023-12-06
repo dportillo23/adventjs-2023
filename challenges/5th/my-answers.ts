26f79924c781cae461524dac53cdd9f0b759f036
Daniel Portillo <dev.portillo23@gmail.com>
2023-12-06 10:59:37 +0100

    Update solution 5

--- challenges/5th/my-answers.ts
+++ challenges/5th/my-answers.ts
@@ -17,3 +17,25 @@ function cyberReindeer(road: string, time: number) {
   }
   return roadStates
 }
+
+/** @score 260 */
+function cyberReindeer2(road: string, time: number) {
+  const roadStates = [road]
+  let currentRoad = road;
+  let sPosition = 0
+  const roadLength = road.length
+  for (let t = 1; t < time; t++) {
+    if (t === 5) {
+      road = road.replaceAll('|', '*')
+    }
+    const nextPosition = sPosition + 1
+    if (road[nextPosition] !== '|') {
+      const before = '.' + road.substring(1, nextPosition)
+      const after = road.substring(nextPosition + 1, roadLength)
+      currentRoad = `${before}S${after}`
+      sPosition = nextPosition
+    }
+    roadStates.push(currentRoad)
+  }
+  return roadStates
+}
