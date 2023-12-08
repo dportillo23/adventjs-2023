0735f263dffd75f64be9c96beaf13f75355cf67d
Daniel Portillo <dev.portillo23@gmail.com>
2023-12-08 10:31:16 +0100

    Update 7th challenge

--- challenges/7th/my-answers.js
+++ challenges/7th/my-answers.js
@@ -60,3 +60,28 @@ function drawGift2(size, symbol) {
     gift += pound.repeat(size - 1) + endLine;
     return gift;
 }
+/** @score 270 */
+function drawGift3(size, symbol) {
+    const endLine = "\n";
+    const pound = "#";
+    if (size === 1) {
+        return pound + endLine;
+    }
+    const lineLength = size * 2 - 1;
+    const firstLine = pound.repeat(size);
+    const upperBox = [" ".repeat(lineLength - size) + firstLine];
+    const bottomBox = [firstLine];
+    const topFace = pound + symbol.repeat(size - 2);
+    for (let i = 2; i < size; i++) {
+        const calc = lineLength - size - i + 1;
+        const prefix = " ".repeat(Math.max(calc, 0));
+        const symbols = symbol.repeat(size - 2 - Math.abs(size - i));
+        const sideFace = pound + symbols + pound;
+        upperBox.push(prefix + topFace + sideFace);
+        bottomBox.unshift(topFace + sideFace);
+    }
+    const symbols = symbol.repeat(lineLength - size - 1);
+    const midLine = pound.repeat(size) + symbols + pound;
+    const newBox = upperBox.concat([midLine, ...bottomBox]);
+    return newBox.join(endLine) + endLine;
+}
